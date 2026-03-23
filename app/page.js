cd5e93101f528c62bda6f0b80e123be3e1e52668
Gashaw Mandie <gashawmandie@gmail.com>
2026-03-23 17:20:43 +0300

    link shared

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // This happens on the server instantly
+  // This happens on the server instantly by redirecting the user to the Play Store page for the Keteraraw app
   redirect("https://play.google.com/store/apps/details?id=com.savvybridge.keteraraw");
 }
